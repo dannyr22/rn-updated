76571836f5eb970c59bffe72a025e413af664e88
Danny Roberts <dannyroberts.b@gmail.com>
2021-08-14 17:03:49 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {StatusBar} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import Home from './src/pages/Home';
+
+const App = () => (
+  <NavigationContainer>
+    <StatusBar barStyle="dark-content" />
+    <Home />
+  </NavigationContainer>
+);
+
+export default App;
