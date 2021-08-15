9eb6ff4d38d925876f3e7d469ed5f0d914579344
Danny Roberts <dannyroberts.b@gmail.com>
2021-08-16 00:57:02 +0100

    created single product FlatList

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,3 +1,105 @@
-const Product = () => null;
+import {Rowing} from '@material-ui/icons';
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  FlatList,
+  Text,
+  View,
+  Image,
+} from 'react-native';
+import {Dimensions} from 'react-native';
+
+// var width = Dimensions.get('window').width; //full width
+// var height = Dimensions.get('window').height; //full height
+
+const Item = ({item}) => (
+  <View style={styles.productContainer}>
+    <View style={styles.productImageContainer}>
+      <Image style={styles.productImage} source={item.imgSource} />
+    </View>
+    <View style={styles.productInfo}>
+      <View style={styles.productText}>
+        <Text style={styles.textPrimary}>{item.title}</Text>
+        <Text style={styles.textPrimary}>£{item.price}</Text>
+        <Text style={styles.textSecondary}>{item.description}</Text>
+      </View>
+      <View>
+        <Image style={styles.productAvatar} source={{uri: item.imgURL}} />
+      </View>
+    </View>
+  </View>
+);
+
+const Product = ({products}) => {
+  const renderItem = ({item}) => <Item item={item} />;
+
+  return (
+    <SafeAreaView>
+      <FlatList
+        data={products}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+    </SafeAreaView>
+  );
+};
 
 export default Product;
+
+const styles = StyleSheet.create({
+  test: {
+    color: 'blue',
+  },
+  image: {
+    width: 50,
+    height: 50,
+  },
+  productContainer: {
+    // borderColor: 'red',
+    // borderWidth: 4,
+    // aspectRatio: 4 / 3,
+  },
+  //   productItem: {},
+  productImage: {
+    width: '100%',
+    height: '100%',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
+  productImageContainer: {
+    aspectRatio: 4 / 3,
+  },
+  productInfo: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    // borderColor: 'red',
+    // borderWidth: 4,
+    width: '100%',
+    resizeMode: 'cover',
+    // height: '100%',
+    // aspectRatio: 4 / 3,
+    backgroundColor: 'rgba(52, 52, 52, 0.8)',
+    padding: 8,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+  },
+  textPrimary: {
+    color: '#FFF',
+    fontWeight: 'bold',
+    // fontFamily: 'IM Fell DW Pica',
+    fontSize: 16,
+    margin: 4,
+  },
+  textSecondary: {
+    color: '#BDBDBD',
+    fontWeight: 'normal',
+    fontSize: 12,
+    margin: 4,
+  },
+  productAvatar: {
+    borderRadius: 50 / 2,
+    height: 50,
+    width: 50,
+  },
+});
--- src/pages/Home/Market.js
+++ src/pages/Home/Market.js
@@ -1,3 +1,110 @@
-const Market = () => null;
+import React, {Component} from 'react';
+import {SafeAreaView, Text, StyleSheet, View} from 'react-native';
+import Product from 'drCodeTest/src/components/Product/index';
 
-export default Market;
+const products = [
+  {
+    id: 1,
+    title: 'toothpaste',
+    price: 2.99,
+    discountAvailable: false,
+    imgSource: require('drCodeTest/assets/images/toothpaste.jpeg'),
+    description: 'I am toothpaste, the BEST toothpaste',
+    imgURL: `https://i.pravatar.cc/`,
+    category: 'cleaning',
+  },
+  {
+    id: 2,
+    title: 'Dino Bear',
+    price: 5.99,
+    discountAvailable: true,
+    imgSource: require('drCodeTest/assets/images/deno-dinosaur.jpeg'),
+    description: 'Amazing Dino teddy bear for puppies',
+    imgURL: `https://i.pravatar.cc/`,
+    category: 'puppy',
+  },
+  {
+    id: 3,
+    title: 'Ping Pong Table',
+    price: 202.99,
+    discountAvailable: true,
+    imgSource: require('drCodeTest/assets/images/tabletennis.jpg'),
+    description: 'Home ping pong table',
+    imgURL: `https://i.pravatar.cc/`,
+    category: 'sports',
+  },
+  {
+    id: 4,
+    title: 'Fabreeze Spray',
+    price: 7.99,
+    discountAvailable: true,
+    imgSource: require('drCodeTest/assets/images/fabreeze.jpeg'),
+    description: 'Amazing air freshner for the home',
+    imgURL: `https://i.pravatar.cc/`,
+    category: 'cleaning',
+  },
+  {
+    id: 5,
+    title: 'Slippers',
+    price: 6.99,
+    discountAvailable: false,
+    imgSource: require('drCodeTest/assets/images/Sherlock.jpeg'),
+    description: 'Chewable slippers for puppies',
+    imgURL: `https://i.pravatar.cc/`,
+    category: 'puppy',
+  },
+  {
+    id: 6,
+    title: 'Tennis shorts',
+    price: 13.99,
+    discountAvailable: false,
+    imgSource: require('drCodeTest/assets/images/tennisshorts.jpeg'),
+    description: 'Very fashionable tennis shorts that turn into trousers!',
+    imgURL: `https://i.pravatar.cc/`,
+    category: 'sports',
+  },
+  {
+    id: 7,
+    title: 'hoover',
+    price: 89.99,
+    discountAvailable: true,
+    imgSource: require('drCodeTest/assets/images/hoover.jpeg'),
+    description:
+      'Bran spanking new hoover created by Byson, the big brother of Dyson',
+    imgURL: `https://i.pravatar.cc/`,
+    category: 'cleaning',
+  },
+  {
+    id: 8,
+    title: 'Squeeky ball set',
+    price: 7.99,
+    discountAvailable: true,
+    imgSource: require('drCodeTest/assets/images/squeeky.jpg'),
+    description: 'Annoying as hell but a very welcome distraction',
+    imgURL: `https://i.pravatar.cc/`,
+    category: 'cleaning',
+  },
+  {
+    id: 9,
+    title: 'Nike Football',
+    price: 24.99,
+    discountAvailable: true,
+    imgSource: require('drCodeTest/assets/images/nike.jpeg'),
+    description: 'Because everyone needs a ball',
+    imgURL: `https://i.pravatar.cc/`,
+    category: 'sports',
+  },
+];
+export default class Market extends Component {
+  render() {
+    return (
+      <SafeAreaView>
+        <View>
+          <Product products={products} />
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
